400325c4e69013d287473c577b3015de8d43b48c
TheRealJackiBoi <jslam@oulund.dk>
2021-04-02 17:35:34 +0200

    try

--- React-first/my-app/src/index.js
+++ React-first/my-app/src/index.js
@@ -1,85 +1,13 @@
-import React, { useRef, useState } from 'react';
-import ReactDOM from 'react-dom';
-
-import reportWebVitals from './reportWebVitals';
-
-import './index.css';
-import './App.css';
-import './Search.css';
-
-import firebase from './firebase.js'
-import 'firebase/auth';
-import 'firebase/analytics';
-import { useAuthState } from 'react-firebase-hooks/auth';
-
-import {Profile} from './components/Profile';
-import {Welcome} from './components/Welcome';
-
-
-const database = firebase.database();
-const auth = firebase.auth();
-const analytics = firebase.analytics();
-
-
-function App() {
   
-  const [user] = useAuthState(auth);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {App} from './App';
+//import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
-    return(<div>
-      
-      <NavBar user={user}/>
-      
-      {user ? <Profile user={1} database={database} /> : <Welcome/> }
-      
-      <img id="fridge-image" src={"/images/Fridge-sticker-final.png"} alt="Fridge Logo" />
-    </div> );
-  }
-
-ReactDOM.render(
-  (
-      <App />
-  ),
-  document.getElementById('root')
-);
-
-
-function SignOut() {
-  return auth.currentUser && (
-      <li><button className="sign-out" onClick={() => auth.signOut()} >Sign Out</button></li>
-    )
-}
-
-function Login() {
-
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-    //check if there's a user with that uid in database, if not: create the user in database
-    /*if (user.uid === null){
-      console.log('error');
-    }*/
-  }
-
-  return (
-      <button className="sign-in" onClick={signInWithGoogle}>Login</button>
-  )
-
-}
-
-function NavBar(props) {
-
-  return(<div className="navBar">
-          <ul>
-            <li id="logo" >Dit Køleskab</li>
-      
-          {props.user ? <SignOut /> : <div><li><Login /></li></div>}
-        </ul>
-    </div>
-  );
-}
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
