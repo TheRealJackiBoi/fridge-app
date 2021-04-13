21771fba90268fcb1b8fb0f876ea789190b1bc45
TheRealJackiBoi <jslam@oulund.dk>
2021-04-13 15:34:02 +0200

    add item display of icons

--- src/components/NewItemMenu.js
+++ src/components/NewItemMenu.js
@@ -2,15 +2,14 @@ import React from 'react';
 import firebase from './../firebase'
 import 'firebase/auth';
 import 'firebase/analytics';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import {} from 'react-firebase-hooks/auth';
 
-export function NewItemMenu() {
+export function NewItemMenu(props) {
 
-    
 
     return (
         <div id="newItemMenu">
-            <button id="extra-plus-button">
+            <button id="close-newItemMenu" onClick={props.addHandler}>
                 <i class="fa fa-minus" aria-hidden="true"></i>
             </button>
             <div id="newitemDisplay">
@@ -18,7 +17,10 @@ export function NewItemMenu() {
                         <input id="searchbar-varer" type="text" placeholder="Søg" ></input>
                     </div>
                 <div id="itemDisplayIcon" >
-                    
+                    <div className="item-display">
+                        <img src="#" alt="" className="item-display-img"/>
+                        <p className="item-display-name" ></p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/UserTD.js
+++ src/components/UserTD.js
@@ -14,6 +14,7 @@ export class UserStorage extends React.Component {
 
       this.onInput = this.onInput.bind(this); 
       this.removeHandler = this.removeHandler.bind(this); 
+      this.addHandler = this.addHandler.bind(this); 
     }
     //work in progress
     onInput(input) {
@@ -34,6 +35,16 @@ export class UserStorage extends React.Component {
       } else {
         this.setState({remove: "true"});
         console.log('true ' + this.state.remove)
+      }
+    }
+
+    addHandler() {
+      if (this.state.add === "true") {
+        this.setState({add: "false"})
+        console.log('false ' + this.state.add)
+      } else {
+        this.setState({add: "true"});
+        console.log('true ' + this.state.add)
       }
     }
 
@@ -72,29 +83,29 @@ export class UserStorage extends React.Component {
             <th className="ud-dato" >Ud-Dato</th>  
             <th className="amount" >Antal</th>  
           </tr>
-        { this.state.varer.map(item => 
-        <tr>
-          <td className="remove-button click"><i class="fa fa-minus" aria-hidden="true"></i></td>
-          <td className="image-display" >
-            <img src={item.picpath} alt={item.picpath}/>
-          </td>
-          <td>{item.name}</td>
-          <td className="ud-dato" >{item.date}</td>
-          <td className="amount">{item.amount}</td>
-        </tr>)
-        }
+            { this.state.varer.map(item => 
+              <tr>
+                <td className="remove-button click"><i class="fa fa-minus" aria-hidden="true"></i></td>
+                <td className="image-display" >
+                  <img src={item.picpath} alt={item.picpath}/>
+                </td>
+                <td>{item.name}</td>
+                <td className="ud-dato" >{item.date}</td>
+                <td className="amount">{item.amount}</td>
+              </tr>)
+            }
         </tbody>
       </table>
       )
     }
     return (
       <table>
-        { this.state.add === "true" ? <NewItemMenu user={this.props.user} database={this.props.database} /> : ""}
+        { this.state.add === "true" ? <NewItemMenu user={this.props.user} database={this.props.database} addHandler={this.addHandler} /> : ""}
         <tbody>
           <tr>
             <td className="image-display">
               <td className="plus-minus-btn click"> 
-                <button id="plus-button">
+                <button id="plus-button" onClick={this.addHandler}>
                   <i class="fa fa-plus" aria-hidden="true"></i>
                 </button>
               </td>
